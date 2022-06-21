0857bc253820acc74a67d1d537f6e8443112ab12
ChairinNabila <105769248+ChairinNabila@users.noreply.github.com>
2022-06-21 22:24:58 +0700

    Tugas 2
    tugas 2 pemrograman berbasis web lanjutan

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+
+    define ('URL', 'http://localhost/project/tugas-2-chairin/');
+    define ('ASSET', URL . 'assets/');
+
+
+    require_once "vendor/autoload.php";
---
+++ pages/dashboard.php
@@ -0,0 +1,28 @@
+<div class="container" style="padding-top: 40px; min-height: 530px">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title">
+                <h5>Tugas 2</h5>
+                <h5>Selamat Datang!</h5>
+            </div>
+            <p class="card-text">
+                <div class="row">
+                    <div class="col-md-1">
+                        Nama 
+                    </div>
+                    <div class="col-md-3">
+                        : Chairin Nabila
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-1">
+                        NIM
+                    </div>
+                    <div class="col-md-3">
+                        : 0702192052
+                    </div>
+                </div>
+            </p>
+        </div>
+    </div>
+</div>
